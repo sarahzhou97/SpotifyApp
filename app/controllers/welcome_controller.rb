b60c0239e913ad2202744b50533119d9b71f3c2a
SARAH ZHOU <sz107@duke.edu>
2017-11-08 23:03:22 -0500

    able to insert into playlist and user table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,16 +12,23 @@ class WelcomeController < ApplicationController
 
   def create_new_user
   	#RSpotify::authenticate("313d26163a7e40cab6a7d08ea0c15df7", "c777ed8256104fd8b28465c61bc56a9c")
-  	spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+  	rspotify_user = RSpotify::User.new(request.env['omniauth.auth'])
 
+  	spotify_user = SpotifyUser.new(:user_id => rspotify_user.id,:name =>rspotify_user.display_name)
+	spotify_user.save
+#:name =>rspotify_user.display_name :birthdate => rspotify_user.birthdate :country => rspotify_user.country
 	# rspotify_user = RSpotify::User.find('12925718')
-	@playlists = spotify_user.playlists
+	playlists = rspotify_user.playlists(limit: 50)
 
-	# for element in playlists do
- #  		puts element.name
-	# end
-  	#get user's name, birthdate,user_id, image
-  	#spotify_user = SpotifyUser.new(:user_id => rspotify_user.user_id :name =>rspotify_user.name :birthdate => rspotify_user.birthdate :image => rspotify_user.image)
+	@user_name = rspotify_user.display_name
+
+	for playlist in playlists do
+  		play = Playlist.new(:playlist_id => playlist.snapshot_id,:creator_id => playlist.owner.id,:playlist_name => playlist.name)
+  		play.save
+	end
+
+
+  	
   	#spotify_user.save
   	#tracks = spotify_user.saved_tracks
 
--- db/migrate/20171108233138_create_spotify_users.rb
+++ db/migrate/20171108233138_create_spotify_users.rb
@@ -2,9 +2,9 @@ class CreateSpotifyUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :spotify_users do |t|
     	t.string :user_id
-    	t.date :birthdate
+    	t.string :birthdate
+    	t.string :country
     	t.string :name
-    	t.string :image
     end
   end
 end
