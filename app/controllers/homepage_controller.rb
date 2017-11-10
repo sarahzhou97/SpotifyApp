0d57cf624ac6f38b4f4a15bd124bc6a664e4abc1
limeymango <emilymeng1@gmail.com>
2017-11-09 22:55:10 -0500

    basic structure for website

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
 
 	def show
-		
+		 
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  
+
 get '/auth/spotify/callback', to: 'welcome#create_new_user'
->>>>>>> d4a0e845640d2dd23e0db8319f673fcfbdbbeb56
+
+get 'homepage/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
