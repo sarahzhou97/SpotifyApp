a2990295e246d454ff8e507b8597116bcd967764
limeymango <emilymeng1@gmail.com>
2017-11-10 00:23:09 -0500

    Set up homepage

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,5 @@
+Chartkick.options = {
+  library: {backgroundColor: "none"}
+}
+#Chartkick.options[:html] = '<div id="%{id}" style="height: %{height};"></div>'
+#Chartkick.options[:content_for] = :charts_js
